1b21a92fc8c08a89503d53ca82905552a4678a77
Victor Santos <menezes.victorsantos@outlook.com>
2026-02-21 20:12:11 -0300

    feature: s3 uploadObject method

--- src/modules/aws/aws.s3.service.ts
+++ src/modules/aws/aws.s3.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { S3Client } from "@aws-sdk/client-s3"
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
 import { ConfigService } from "@nestjs/config";
 import { TypedEnv } from "config/env";
 
@@ -7,15 +7,18 @@ import { TypedEnv } from "config/env";
 export class S3Service extends S3Client {
     constructor(configService: ConfigService<TypedEnv>) {
         super({
-            credentials: {
-                accessKeyId: configService.getOrThrow("S3_ACCESS_KEY"),
-                secretAccessKey: configService.getOrThrow("S3_SECRET_KEY")
-            }
+            region: "sa-east-1"
         })
     }
     private logger = new Logger(S3Service.name)
-    uploadObject(object: string) {
-
+    async uploadObject(object: string) {
+        await this.send(new PutObjectCommand({
+            Bucket: "comprez",
+            Key: "teste/teste.txt",
+            Body: object
+        }))
     }
+
+
 
 }
